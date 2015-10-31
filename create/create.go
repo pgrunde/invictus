27af044eb7c923c0d84abc9e5374f2b580f841f7
Peter Grunde <peter.a.grunde@gmail.com>
2015-10-31 14:49:00 -0600

    replaced hasIllegalFilename return with a reportable error

--- create/create.go
+++ create/create.go
@@ -7,31 +7,32 @@ import (
 
 var badWindowsNames = []string{"com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9", "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9", "con", "nul", "prn"}
 
-var badCharacters = []rune{'/', '?', '<', '>', '\\', ':', '*', '|', '"', '.'}
+var badCharacters = []rune{'^', '/', '?', '<', '>', '\\', ':', '*', '|', '"', '.'}
 
 func Project(s string) error {
 	s = strings.ToLower(s)
-	if hasIllegalFilename(s) {
-		return fmt.Errorf("Given project name %s has an illegal filename", s)
+	if err := hasIllegalFilename(s); err != nil {
+		return fmt.Errorf("Given project name %s has an illegal filename: %s", s, err)
 	}
 	return nil
 }
 
-// / ? < > \ : * | " . com1, com2, com3, com4, com5, com6, com7, com8, com9, lpt1, lpt2, lpt3, lpt4, lpt5, lpt6, lpt7, lpt8, lpt9, con, nul, and prn
-// must be <= 255 characters long
-func hasIllegalFilename(s string) bool {
+func hasIllegalFilename(s string) error {
+	if s == "" {
+		return fmt.Errorf("No project name given. Please specify a project name")
+	}
 	if len(s) > 255 {
-		return true
+		return fmt.Errorf("Project name cannot exceed 255 characters")
 	}
 	for _, bwn := range badWindowsNames {
 		if s == bwn {
-			return true
+			return fmt.Errorf("Project name contains a reserved Windows operating system file name")
 		}
 	}
 	for _, r := range badCharacters {
 		if strings.ContainsRune(s, r) {
-			return true
+			return fmt.Errorf("Found illegal character %s in filename", string(r))
 		}
 	}
-	return false
+	return nil
 }
