1613f1903df6d1f232a38ff19710d028ebb93b7e
Peter Grunde <grunde@Peters-MacBook-Pro.local>
2016-01-04 22:14:04 -0700

    main template filled to create logfiles and start server

--- create/templates/maintemp.go
+++ create/templates/maintemp.go
@@ -9,11 +9,72 @@ import (
 const mainTemplateText = `package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/pgrunde/img/server"
+
+	"github.com/aodin/sol"
+	_ "github.com/aodin/sol/postgres"
+	"github.com/aodin/volta/config"
+	"github.com/codegangsta/cli"
 )
 
 func main() {
-	fmt.Println("{{.ProjectName}}")
+	app := cli.NewApp()
+	app.Name = "img"
+	app.Usage = "runs the image manipulation server"
+	app.Version = server.VERSION
+	app.Action = startServer
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "log, l",
+			Value: "",
+			Usage: "Sets the log output file path",
+		},
+		cli.StringFlag{
+			Name:  "config, c",
+			Value: "./settings.json",
+			Usage: "Sets the configuration file",
+		},
+	}
+	app.Run(os.Args)
+}
+
+func startServer(c *cli.Context) {
+	logF := c.String("log")
+	file := c.String("config")
+	// Set the log output - if no path given, use stdout
+	if logF != "" {
+		if err := os.MkdirAll(filepath.Dir(logF), 0776); err != nil {
+			log.Panic(err)
+		}
+		l, err := os.OpenFile(logF, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			log.Panic(err)
+		}
+		defer l.Close()
+		log.SetOutput(l)
+	}
+	conf, conn := Connect(file)
+	defer conn.Close()
+	log.Panic(server.New(conf, conn).ListenAndServe())
+}
+
+func Connect(file string) (config.Config, sol.Conn) {
+	// Parse the given configuration file
+	conf, err := config.ParseFile(file)
+	if err != nil {
+		log.Panicf("img: could not parse configuration: %s", err)
+	}
+
+	// Connect to the database
+	conn, err := sol.Open(conf.Database.Driver, conf.Database.Credentials())
+	if err != nil {
+		log.Panicf("img: could not connect to the database: %s", err)
+	}
+	return conf, conn
 }
 `
 
