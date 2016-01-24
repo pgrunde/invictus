cbd3416bafc007a5fe3b61024491539327a1841b
pgrunde <peter.a.grunde@gmail.com>
2016-01-24 00:52:04 -0700

    dbconf generates, name change templates

---
+++ create/templates/dbconf.go
@@ -0,0 +1,41 @@
+package templates
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/template"
+)
+
+const dbconfTemplateText = `development:
+    driver: postgres
+    open: host=localhost port=5432 dbname={{.DbName}}_dev user={{.PostgresUser}} password={{.PostgresPassword}} sslmode=disable
+
+testing:
+    driver: postgres
+    open: host=localhost port=5432 dbname={{.DbName}}_test user={{.PostgresUser}} password={{.PostgresPassword}} sslmode=disable
+`
+
+var dbconfTemplate = template.Must(
+	template.New("dbconf").Parse(dbconfTemplateText),
+)
+
+func CreateDbConf(projectName, fullpath, dbname string) {
+	attr := struct {
+		ProjectName string
+		DbName string
+		PostgresUser string
+		PostgresPassword string
+	}{
+		ProjectName: projectName,
+		DbName: dbname,
+		PostgresUser: "peter",
+		PostgresPassword: "password",
+	}
+	file, err := os.Create(fmt.Sprintf("%s/%s", fullpath, projectName) + "/db/dbconf.yml")
+	if err != nil {
+		log.Fatal("cannot create a dbconf.yml file")
+	}
+	defer file.Close()
+	dbconfTemplate.Execute(file, attr)
+}
