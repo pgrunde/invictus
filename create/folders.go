fa409acd166d950df76caf8e9766cba68b8faf14
pgrunde <peter.a.grunde@gmail.com>
2016-02-23 22:54:21 -0700

    refactored folders

--- create/folders.go
+++ create/folders.go
@@ -7,36 +7,28 @@ import (
 )
 
 func createDbFolder(s, path string) {
-	err := os.Mkdir(fmt.Sprintf("%s/%s/db", path, s), 0744)
-	if err != nil {
-		log.Fatalf("cannot create a /db folder at path %s%s: %s", path, s, err)
-	}
+	createFolder(s, path, "db")
 }
 
 func createMigrationsFolder(s, path string) {
-	err := os.Mkdir(fmt.Sprintf("%s/%s/db/migrations", path, s), 0744)
-	if err != nil {
-		log.Fatalf("cannot create a /db/migrations folder at path %s%s: %s", path, s, err)
-	}
+	createFolder(s, path, "db/migrations")
 }
 
 func createServerFolder(s, path string) {
-	err := os.Mkdir(fmt.Sprintf("%s/%s/server", path, s), 0744)
-	if err != nil {
-		log.Fatalf("cannot create a /server folder at path %s%s: %s"+path+s, err)
-	}
+	createFolder(s, path, "server")
 }
 
 func createApiFolder(s, path string) {
-	err := os.Mkdir(fmt.Sprintf("%s/%s/server/api", path, s), 0744)
-	if err != nil {
-		log.Fatalf("cannot create a /server/api folder at path %s%s: %s", path, s, err)
-	}
+	createFolder(s, path, "server/api")
 }
 
 func createParamsFolder(s, path string) {
-	err := os.Mkdir(fmt.Sprintf("%s/%s/server/params", path, s), 0744)
+	createFolder(s, path, "server/params")
+}
+
+func createFolder(s, path, folder string) {
+	err := os.Mkdir(fmt.Sprintf("%s/%s/"+folder, path, s), 0744)
 	if err != nil {
-		log.Fatalf("cannot create a /server/params folder at path %s%s: %s", path, s, err)
+		log.Fatalf("cannot create a /%s folder at path %s%s: %s", folder, path, s, err)
 	}
 }
