9605b52ad7849c141a232095ac76cd60ec8cf55f
pgrunde <peter.a.grunde@gmail.com>
2016-03-21 08:10:40 -0600

    server.Start moved into server package

--- create/templates/maintemp.go
+++ create/templates/maintemp.go
@@ -9,15 +9,10 @@ import (
 const mainTemplateText = `package main
 
 import (
-	"log"
 	"os"
-	"path/filepath"
 
 	"{{.ProjectInGopath}}/server"
 
-	"github.com/aodin/sol"
-	_ "github.com/aodin/sol/postgres"
-	"github.com/aodin/volta/config"
 	"github.com/codegangsta/cli"
 )
 
@@ -26,7 +21,7 @@ func main() {
 	app.Name = "img"
 	app.Usage = "runs the image manipulation server"
 	app.Version = server.VERSION
-	app.Action = startServer
+	app.Action = server.Start
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "log, l",
@@ -40,41 +35,6 @@ func main() {
 		},
 	}
 	app.Run(os.Args)
-}
-
-func startServer(c *cli.Context) {
-	logF := c.String("log")
-	file := c.String("config")
-	// Set the log output - if no path given, use stdout
-	if logF != "" {
-		if err := os.MkdirAll(filepath.Dir(logF), 0776); err != nil {
-			log.Panic(err)
-		}
-		l, err := os.OpenFile(logF, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-		if err != nil {
-			log.Panic(err)
-		}
-		defer l.Close()
-		log.SetOutput(l)
-	}
-	conf, conn := Connect(file)
-	defer conn.Close()
-	log.Panic(server.New(conf, conn).ListenAndServe())
-}
-
-func Connect(file string) (config.Config, sol.Conn) {
-	// Parse the given configuration file
-	conf, err := config.ParseFile(file)
-	if err != nil {
-		log.Panicf("{{.ProjectName}}: could not parse configuration: %s", err)
-	}
-
-	// Connect to the database
-	conn, err := sol.Open(conf.Database.Driver, conf.Database.Credentials())
-	if err != nil {
-		log.Panicf("{{.ProjectName}}: could not connect to the database: %s", err)
-	}
-	return conf, conn
 }
 `
 
--- create/templates/server.go
+++ create/templates/server.go
@@ -30,12 +30,16 @@ const serverTemplateText = `package server
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
-	"{{.ProjectInGopath}}/server/api"
 	"{{.ProjectInGopath}}/v1"
+	"{{.ProjectInGopath}}/server/api"
 
 	"github.com/aodin/sol"
+	_ "github.com/aodin/sol/postgres"
 	"github.com/aodin/volta/config"
+	"github.com/codegangsta/cli"
 )
 
 const VERSION = "0.0.1"
@@ -65,6 +69,41 @@ func New(conf config.Config, conn sol.Conn) *Server {
 func (server *Server) ListenAndServe() error {
 	log.Printf("server: serving on address %s\n", server.Config.Address())
 	return http.ListenAndServe(server.Config.Address(), nil)
+}
+
+func Start(c *cli.Context) {
+	logF := c.String("log")
+	file := c.String("config")
+	// Set the log output - if no path given, use stdout
+	if logF != "" {
+		if err := os.MkdirAll(filepath.Dir(logF), 0776); err != nil {
+			log.Panic(err)
+		}
+		l, err := os.OpenFile(logF, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			log.Panic(err)
+		}
+		defer l.Close()
+		log.SetOutput(l)
+	}
+	conf, conn := Connect(file)
+	defer conn.Close()
+	log.Panic(New(conf, conn).ListenAndServe())
+}
+
+func Connect(file string) (config.Config, sol.Conn) {
+	// Parse the given configuration file
+	conf, err := config.ParseFile(file)
+	if err != nil {
+		log.Panicf("img: could not parse configuration: %s", err)
+	}
+
+	// Connect to the database
+	conn, err := sol.Open(conf.Database.Driver, conf.Database.Credentials())
+	if err != nil {
+		log.Panicf("img: could not connect to the database: %s", err)
+	}
+	return conf, conn
 }
 
 func favicon(w http.ResponseWriter, r *http.Request) {
